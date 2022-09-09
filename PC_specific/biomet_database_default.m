14c11d800d27865689ae481c7d3a412c9b4c0821
znesic <z_nesic@hotmail.com>
2022-09-09 15:36:50 -0700

    Prepared the template (draft #1)

--- PC_specific/biomet_database_default.m
+++ PC_specific/biomet_database_default.m
@@ -1,2 +1,19 @@
 function x = biomet_database_default
-x = 'p:\database\';
+% Micromet database default path
+
+% Edit the lines below to make it work on your PC.
+if ispc
+    % if you are connected to Geog VPN and working from home:
+    x = '\\vinimet.geog.ubc.ca\Database\';
+    % if you are working on your local copy of database 
+    % and you have it in your Documents folder:
+    % x = 'C:\Users\your_name\Documents\Database'
+elseif ismac
+    % if you are on a Mac and have mounted remote 
+    % drive vinimet.geog.ubc.ca/Database
+    x = '/Volumes/Database/';
+    % if you are working on your local copy of database 
+    % and you have it in your Library folder:
+    %x = '/Users/your_name/Library/Database/';
+end
+
--- PC_specific/user_set.m
+++ PC_specific/user_set.m
@@ -1,3 +1,11 @@
-user_dir = 'C:\Ubc_flux\Matlab\';
-server = 1;
+server = 0;
 warning('off','MATLAB:dispatcher:InexactMatch')
+
+% Edit the lines below to make it work on your PC.
+if ispc
+    % your default folder for Matlab files on your PC
+    user_dir = 'C:\Users\your_name\Documents\Matlab';
+elseif ismac
+    % your default folder for Matlab files on your PC
+    x = '/Users/your_name/Library/Matlab/';
+end
